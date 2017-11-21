d35f9d688d28af029efd8decec807804d93e7ca2
John Hawthorn <john.hawthorn@gmail.com>
2017-11-21 11:49:08 -0800

    Link in stock transfer assets

--- app/assets/javascripts/spree/backend/solidus_stock_transfers.js
+++ app/assets/javascripts/spree/backend/solidus_stock_transfers.js
@@ -1,2 +1,9 @@
-// Placeholder manifest file.
-// the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/backend/all.js'
+//= require spree/backend/templates/stock_transfers/transfer_item
+//= require spree/backend/stock_transfers/count_update_forms
+//= require spree/backend/stock_transfers/edit
+//= require spree/backend/stock_transfers/receive
+//= require spree/backend/stock_transfers/received_counter
+//= require spree/backend/stock_transfers/stock_transfer
+//= require spree/backend/stock_transfers/transfer_item
+//= require spree/backend/stock_transfers/transfer_item_deleting
+//= require spree/backend/stock_transfers/variant_form
--- lib/generators/solidus_stock_transfers/install/install_generator.rb
+++ lib/generators/solidus_stock_transfers/install/install_generator.rb
@@ -4,12 +4,10 @@ module SolidusStockTransfers
       class_option :auto_run_migrations, type: :boolean, default: false
 
       def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/solidus_stock_transfers\n"
         append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/solidus_stock_transfers\n"
       end
 
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/solidus_stock_transfers\n", before: /\*\//, verbose: true
         inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/solidus_stock_transfers\n", before: /\*\//, verbose: true
       end
 
