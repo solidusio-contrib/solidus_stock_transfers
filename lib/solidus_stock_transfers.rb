a5fdc21b06f7688025e64600460fc8dd9edf4d3c
John Hawthorn <john@stembolt.com>
2017-11-21 11:18:24 -0800

    Require permission sets

--- lib/solidus_stock_transfers.rb
+++ lib/solidus_stock_transfers.rb
@@ -1,2 +1,8 @@
 require 'solidus_core'
+
+require 'spree/permission_sets/restricted_stock_transfer_management'
+require 'spree/permission_sets/restricted_stock_transfer_display'
+require 'spree/permission_sets/stock_transfer_display'
+require 'spree/permission_sets/stock_transfer_management'
+
 require 'solidus_stock_transfers/engine'
