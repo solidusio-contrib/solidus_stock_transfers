e338337b53d5fe6a4aab6fd1de6dd921fd85b575
John Hawthorn <john@stembolt.com>
2017-11-21 00:32:56 -0800

    Use example_status_persistence_file_path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,4 +20,6 @@ Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
+
+  config.example_status_persistence_file_path = "spec/examples.txt"
 end
